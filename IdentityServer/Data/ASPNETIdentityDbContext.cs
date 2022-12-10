d79fb2069c4ecca52c128354582f6c90e9932458
regie <regie@callhandling.co.uk>
2022-12-10 18:08:03 +0800

    Add project files.

---
+++ IdentityServer/Data/ASPNETIdentityDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.Data
+{
+    public class AspNetIdentityDbContext : IdentityDbContext
+    {
+        public AspNetIdentityDbContext(DbContextOptions<AspNetIdentityDbContext> options) : base(options) { }
+
+    }
+}
